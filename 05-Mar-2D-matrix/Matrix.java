6ca87716d3a6c27a2ad1adb90695c8fe022bee95
Quazi Yadgar Husain <72057700+quaziyadgar@users.noreply.github.com>
2022-03-09 23:57:28 +0530

    Add files via upload

---
+++ 05-Mar-2D-matrix/Matrix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Matrix
+ */
+public class Matrix {
+    static void display(int[][] matrix,int r,int c)
+    {
+        for(int i = 0; i<r; i++){
+            for(int j = 0; j<c; j++)
+            System.out.print(matrix[i][j]+" "); 
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row and column length for a matrix creation...");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] matrix = new int [r][c];
+        System.out.println("Insert element in matrix");
+        for(int i = 0; i<r; i++)
+        for(int j = 0; j<c; j++)
+            matrix[i][j] = sc.nextInt();
+            display(matrix, r, c);
+            sc.close();
+    }
+}
---
+++ 05-Mar-2D-matrix/StringSearchMatrix.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class StringSearchMatrix {
+
+
+    static void stringSearchMatrix(char[][] matrix,int r,int c, String str)
+    {
+        char[] charStr = str.toCharArray();
+        int s = 0;
+        int count = 0;
+        while(s<charStr.length)
+        {
+            int n = s;
+            outer:
+            for(int i = 0; i<r; i++){
+                for(int j = 0; j<c; j++)
+                {
+                    if(charStr[s] == matrix[i][j])
+                    {
+                        count++;
+                        matrix[i][j] = '0';
+                        s++;
+                        break outer;
+                    }
+                }
+            }
+            if(n++ != s)
+            break;
+        }
+        if(count == charStr.length)
+        System.out.println("String Present");
+        else
+        System.out.println("String Not Present");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row and column length for a matrix creation...");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        char[][] matrix = new char [r][c];
+        System.out.println("Insert element in matrix");
+        for(int i = 0; i<r; i++)
+        for(int j = 0; j<c; j++)
+            matrix[i][j] = sc.next().charAt(0);
+        System.out.println("Enter a string");
+        String str = sc.next();
+            stringSearchMatrix(matrix, r, c, str);
+            sc.close();
+    }
+}
